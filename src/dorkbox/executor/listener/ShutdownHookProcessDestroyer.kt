6eeb94a8b72260fadd5ea602ce8ec01294f426c2
Robinson <robinson@dorkbox.com>
2023-06-07 23:57:53 +0200

    Safer addition of shutdown hook

--- src/dorkbox/executor/listener/ShutdownHookProcessDestroyer.kt
+++ src/dorkbox/executor/listener/ShutdownHookProcessDestroyer.kt
@@ -1,40 +1,17 @@
 /*
- * Copyright 2020 dorkbox, llc
- * Copyright (C) 2014 ZeroTurnaround <support@zeroturnaround.com>
- * Contains fragments of code from Apache Commons Exec, rights owned
- * by Apache Software Foundation (ASF).
+ * Copyright 2023 dorkbox, llc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * NOTICE: This file originates from the Apache Commons Exec package.
- * It has been modified to fit our needs.
- *
- * The following is the original header of the file in Apache Commons Exec:
- *
- *   Licensed to the Apache Software Foundation (ASF) under one or more
- *   contributor license agreements.  See the NOTICE file distributed with
- *   this work for additional information regarding copyright ownership.
- *   The ASF licenses this file to You under the Apache License, Version 2.0
- *   (the "License"); you may not use this file except in compliance with
- *   the License.  You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
  */
 
 package dorkbox.executor.listener
@@ -119,8 +96,11 @@ class ShutdownHookProcessDestroyer : ProcessDestroyer, Runnable {
     private fun addShutdownHook() {
         if (!running) {
             destroyProcessThread = ProcessDestroyerImpl()
-            Runtime.getRuntime().addShutdownHook(destroyProcessThread)
-            isAddedAsShutdownHook = true
+            try {
+                Runtime.getRuntime().addShutdownHook(destroyProcessThread)
+                isAddedAsShutdownHook = true
+            } catch (ignored: Exception) {
+            }
         }
     }
 
@@ -136,7 +116,7 @@ class ShutdownHookProcessDestroyer : ProcessDestroyer, Runnable {
             }
 
             /*
-             * start the hook thread, a un-started thread may not be eligible for garbage collection
+             * start the hook thread, an un-started thread may not be eligible for garbage collection
              *
              * see: http://developer.java.sun.com/developer/bugParade/bugs/4533087.html
              */
